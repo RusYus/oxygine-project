e1a270c46928c4c4ce10f531a170c3690b78c39c
Ruslan Yusupov <ruslan.yusupov@ntprog.com>
2017-09-12 19:01:35 +0300

    [MOV2-47] Code cleanup + velocity.

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -22,163 +22,51 @@ private:
 struct Normal2 : oxygine::Vector2
 {
 public:
-    Normal2()
-        : oxygine::Vector2()
-    {}
-
-    Normal2(float x, float y)
-        : oxygine::Vector2(x, y)
-    {}
-
-    void operator =(const oxygine::Vector2& a_Vector)
-    {
-        x = a_Vector.x;
-        y = a_Vector.y;
-    }
-
-    void operator += (const oxygine::Vector2& a_Vector)
-    {
-        x += a_Vector.x;
-        y += a_Vector.y;
-        FixCoordinatesIfExceeds();
-    }
-
-    void operator += (const Normal2& a_Vector)
-    {
-        x += a_Vector.x;
-        y += a_Vector.y;
-        FixCoordinatesIfExceeds();
-    }
-
-    void operator -= (const oxygine::Vector2& a_Vector)
-    {
-        x -= a_Vector.x;
-        y -= a_Vector.y;
-        FixCoordinatesIfExceeds();
-    }
-
-    void operator -= (const Normal2& a_Vector)
-    {
-        x -= a_Vector.x;
-        y -= a_Vector.y;
-        FixCoordinatesIfExceeds();
-    }
-
-    bool operator == (const Normal2& a_Vector)
-    {
-        return x == a_Vector.x && y == a_Vector.y;
-    }
-
-    bool operator != (const Normal2& a_Vector)
-    {
-        return !this->operator ==(a_Vector);
-    }
-
-    inline void FixCoordinatesIfExceeds()
-    {
-        if (x < -1)
-        {
-            x = -1;
-        }
-
-        if (x > 1)
-        {
-            x = 1;
-        }
-
-        if (y < -1)
-        {
-            y = -1;
-        }
-
-        if (y > 1)
-        {
-            y = 1;
-        }
-    }
+    Normal2();
+    Normal2(float x, float y);
+    void operator =(const oxygine::Vector2& a_Vector);
+    void operator += (const oxygine::Vector2& a_Vector);
+    void operator += (const Normal2& a_Vector);
+    void operator -= (const oxygine::Vector2& a_Vector);
+    void operator -= (const Normal2& a_Vector);
+    bool operator == (const Normal2& a_Vector);
+    bool operator != (const Normal2& a_Vector);
+    inline void FixCoordinatesIfExceeds();
 };
 
 struct Velocity : oxygine::Vector2
 {
 public:
-    Velocity()
-        : oxygine::Vector2()
-    {}
-
-    Velocity(float a_X, float a_Y)
-        : oxygine::Vector2(a_X, a_Y)
-    {}
-
-    Velocity(const oxygine::Vector2& a_Vector)
-        : oxygine::Vector2(a_Vector.x, a_Vector.y)
-    {}
-
-    Velocity operator + (const VectorT2& a_Vector) const
-    {
-        Velocity temp = oxygine::Vector2::operator +(a_Vector);
-        // TODO : rounding
-        return temp;
-    }
-
-    Velocity operator - (const VectorT2& a_Vector) const
-    {
-        Velocity temp = oxygine::Vector2::operator -(a_Vector);
-        // TODO : rounding
-        return temp;
-    }
-
+    Velocity();
+    Velocity(float a_X, float a_Y);
+    Velocity(const oxygine::Vector2& a_Vector);
+    Velocity operator + (const VectorT2& a_Vector) const;
+    Velocity operator - (const VectorT2& a_Vector) const;
     template <class R>
-    Velocity operator * (R a_Vector) const
-    {
-        Velocity temp = oxygine::Vector2::operator *(a_Vector);
-        // TODO : rounding
-        return temp;
-    }
-
+    Velocity operator * (R a_Vector) const;
     template <class R>
-    Velocity operator / (R a_Vector) const
-    {
-        Velocity temp = oxygine::Vector2::operator /(a_Vector);
-        // TODO : rounding
-        return temp;
-    }
-
-    Velocity& operator += (const VectorT2& a_Vector)
-    {
-        oxygine::Vector2::operator +=(a_Vector);
-        // TODO : rounding
-        return (*this);
-    }
-
-    Velocity& operator -= (const VectorT2& a_Vector)
-    {
-        oxygine::Vector2::operator -=(a_Vector);
-        // TODO : rounding
-        return (*this);
-    }
-
+    Velocity operator / (R a_Vector) const;
+    Velocity& operator += (const VectorT2& a_Vector);
+    Velocity& operator -= (const VectorT2& a_Vector);
     template <typename T>
-    Velocity& operator *= (T a_Value)
-    {
-        oxygine::Vector2::operator *=(a_Value);
-        // TODO : rounding
-        return (*this);
-    }
-
+    Velocity& operator *= (T a_Value);
     template <typename T>
-    Velocity& operator /= (T a_Value)
-    {
-        oxygine::Vector2::operator /=(a_Value);
-        // TODO : rounding
-        return (*this);
-    }
+    Velocity& operator /= (T a_Value);
+    // TODO :
+//    struct X
+//    {
+//        float value;
+//        template <typename T>
+//        void operator +=(T a_Value)
+//        {
+//            value += a_Value;
+//        }
+//    };
+//    X x, y;
 };
 
 static const Normal2 ZeroNormal = Normal2{0, 0};
-
-float RoundToNDigits(float, int);
-
-void RoundToNDigits(oxygine::Vector2&, int);
-void RoundToNDigits(oxygine::Point&, int);
-
+float RoundToNDigits(float, int /*a_N*/= 2);
+void RoundToNDigits(oxygine::Vector2&, int /*a_N*/= 2);
+void RoundToNDigits(Velocity&, int /*a_N*/= 2);
 }
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -3,9 +3,156 @@
 namespace Service
 {
 
+Normal2::Normal2()
+    : oxygine::Vector2()
+{}
+
+Normal2::Normal2(float x, float y)
+    : oxygine::Vector2(x, y)
+{}
+
+void Normal2::operator =(const oxygine::Vector2& a_Vector)
+{
+    x = a_Vector.x;
+    y = a_Vector.y;
+}
+
+void Normal2::operator += (const oxygine::Vector2& a_Vector)
+{
+    x += a_Vector.x;
+    y += a_Vector.y;
+    FixCoordinatesIfExceeds();
+}
+
+void Normal2::operator += (const Normal2& a_Vector)
+{
+    x += a_Vector.x;
+    y += a_Vector.y;
+    FixCoordinatesIfExceeds();
+}
+
+void Normal2::operator -= (const oxygine::Vector2& a_Vector)
+{
+    x -= a_Vector.x;
+    y -= a_Vector.y;
+    FixCoordinatesIfExceeds();
+}
+
+void Normal2::operator -= (const Normal2& a_Vector)
+{
+    x -= a_Vector.x;
+    y -= a_Vector.y;
+    FixCoordinatesIfExceeds();
+}
+
+bool Normal2::operator == (const Normal2& a_Vector)
+{
+    return x == a_Vector.x && y == a_Vector.y;
+}
+
+bool Normal2::operator != (const Normal2& a_Vector)
+{
+    return !this->operator ==(a_Vector);
+}
+
+inline void Normal2::FixCoordinatesIfExceeds()
+{
+    if (x < -1)
+    {
+        x = -1;
+    }
+
+    if (x > 1)
+    {
+        x = 1;
+    }
+
+    if (y < -1)
+    {
+        y = -1;
+    }
+
+    if (y > 1)
+    {
+        y = 1;
+    }
+}
+
+Velocity::Velocity()
+    : oxygine::Vector2()
+{}
+
+Velocity::Velocity(float a_X, float a_Y)
+    : oxygine::Vector2(a_X, a_Y)
+{}
+
+Velocity::Velocity(const oxygine::Vector2& a_Vector)
+    : oxygine::Vector2(a_Vector.x, a_Vector.y)
+{}
+
+Velocity Velocity::operator + (const VectorT2& a_Vector) const
+{
+    Velocity temp = oxygine::Vector2::operator +(a_Vector);
+    RoundToNDigits(temp);
+    return temp;
+}
+
+Velocity Velocity::operator - (const VectorT2& a_Vector) const
+{
+    Velocity temp = oxygine::Vector2::operator -(a_Vector);
+    RoundToNDigits(temp);
+    return temp;
+}
+
+template <class R>
+Velocity Velocity::operator * (R a_Vector) const
+{
+    Velocity temp = oxygine::Vector2::operator *(a_Vector);
+    RoundToNDigits(temp);
+    return temp;
+}
+
+template <class R>
+Velocity Velocity::operator / (R a_Vector) const
+{
+    Velocity temp = oxygine::Vector2::operator /(a_Vector);
+    RoundToNDigits(temp);
+    return temp;
+}
+
+Velocity& Velocity::operator += (const VectorT2& a_Vector)
+{
+    oxygine::Vector2::operator +=(a_Vector);
+    RoundToNDigits(*this);
+    return (*this);
+}
+
+Velocity& Velocity::operator -= (const VectorT2& a_Vector)
+{
+    oxygine::Vector2::operator -=(a_Vector);
+    RoundToNDigits(*this);
+    return (*this);
+}
+
+template <typename T>
+Velocity& Velocity::operator *= (T a_Value)
+{
+    oxygine::Vector2::operator *=(a_Value);
+    RoundToNDigits(*this);
+    return (*this);
+}
+
+template <typename T>
+Velocity& Velocity::operator /= (T a_Value)
+{
+    oxygine::Vector2::operator /=(a_Value);
+    RoundToNDigits(*this);
+    return (*this);
+}
+
 IdGenerator::TId IdGenerator::mId = 0;
 
-float RoundToNDigits(float a_Number, int a_N = 2)
+float RoundToNDigits(float a_Number, int a_N)
 {
     if (a_N < 1 || a_N > 4)
     {
@@ -15,13 +162,13 @@ float RoundToNDigits(float a_Number, int a_N = 2)
     return std::round(a_Number * ratio) / ratio;
 }
 
-void RoundToNDigits(oxygine::Vector2& a_Vector, int a_N = 2)
+void RoundToNDigits(oxygine::Vector2& a_Vector, int a_N)
 {
     a_Vector.x = RoundToNDigits(a_Vector.x, a_N);
     a_Vector.x = RoundToNDigits(a_Vector.y, a_N);
 }
 
-void RoundToNDigits(Velocity& a_Vector, int a_N = 2)
+void RoundToNDigits(Velocity& a_Vector, int a_N)
 {
     a_Vector.x = RoundToNDigits(a_Vector.x, a_N);
     a_Vector.x = RoundToNDigits(a_Vector.y, a_N);
